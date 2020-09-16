6835c5d6ff6fe7f8f39045121ed709ee733d3309
Alessandro Braidotti <alessandro@kintyre.co>
2020-09-16 10:32:04 -0400

    add item interface and services

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -6,28 +6,37 @@ import {
   Param,
   Post,
   Put,
-  Req,
-  Res,
+  // Req,
+  // Res,
 } from '@nestjs/common';
 import { CreateItemDto } from './DTO/create-item.dto';
-import { Request, Response } from 'express';
+// import { Request, Response } from 'express';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
-  //  GET localhost:3000/items
-  @Get()
-  findAll(@Req() req: Request, @Res() res: Response): Response {
-    // the default return is below, but let's use Express's Response function instead
-    // return 'Get all items';
+  constructor(private readonly itemsService: ItemsService) {}
 
-    console.log('request url: ', req.url);
-    return res.send('Hello World');
+  @Get()
+  findAll(): Item[] {
+    return this.itemsService.findAll();
   }
+
+  // //  GET localhost:3000/items
+  // @Get()
+  // findAll(@Req() req: Request, @Res() res: Response): Response {
+  //   // the default return is below, but let's use Express's Response function instead
+  //   // return 'Get all items';
+
+  //   console.log('request url: ', req.url);
+  //   return res.send('Hello World');
+  // }
 
   //  GET localhost:3000/items/1
   @Get(':id')
-  findOne(@Param('id') id): string {
-    return `Item ${id}`;
+  findOne(@Param('id') id): Item {
+    return this.itemsService.findOne(id);
   }
 
   //   POST to localhost:3000/items
---
+++ src/items/items.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
+
+@Injectable()
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: '1',
+      name: 'Item One',
+      quantity: 100,
+      description: 'This is Item One',
+    },
+    {
+      id: '2',
+      name: 'Item Two',
+      quantity: 50,
+      description: 'This is item Two',
+    },
+  ];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findOne(id: string): Item {
+    return this.items.find(item => item.id === id);
+  }
+}
