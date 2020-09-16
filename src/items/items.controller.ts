9f78c0257151f86d9a7ca02b4092353359a2b86f
Alessandro Braidotti <alessandro@kintyre.co>
2020-09-16 10:11:09 -0400

    initial commit with items controller

---
+++ src/items/items.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { CreateItemDto } from './DTO/create-item.dto';
+import { Request, Response } from 'express';
+
+@Controller('items')
+export class ItemsController {
+  //  GET localhost:3000/items
+  @Get()
+  findAll(@Req() req: Request, @Res() res: Response): Response {
+    // the default return is below, but let's use Express's Response function instead
+    // return 'Get all items';
+
+    console.log('request url: ', req.url);
+    return res.send('Hello World');
+  }
+
+  //  GET localhost:3000/items/1
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
+  }
+
+  //   POST to localhost:3000/items
+  //   body:
+  //    {
+  //        "name": "Item One",
+  //        "description": "This is the first item",
+  //        "quantity": 100
+  //    }
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
+
+  //   DELETE to localhost:3000/items
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `Delete ${id}`;
+  }
+
+  //   POST to localhost:3000/items/1
+  //    {
+  //        "name": "Item One",
+  //        "description": "This is the first item",
+  //        "quantity": 101
+  //    }
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `Update ${id} - Name: ${updateItemDto.name}`;
+  }
+}
